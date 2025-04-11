195093090ec00d0fa41e0d3a4409bc2c37d0d14e
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2025-04-11 19:52:37 +0200

    Update arduino_port_expander.cpp

--- components/arduino_port_expander/arduino_port_expander.cpp
+++ components/arduino_port_expander/arduino_port_expander.cpp
@@ -62,8 +62,10 @@ bool ArduinoPortExpanderComponent::digital_read(uint8_t pin) {
 
 uint8_t buf[1] = {pin};
 
- this->write_register(CMD_DIGITAL_READ, buf, 1, false);
-  success = (this->read_register(pin, this->read_buffer_, 1) == i2c::ERROR_OK);
+ //this->write_register(CMD_DIGITAL_READ, buf, 1, false);
+  this->write_register(CMD_DIGITAL_READ, nullptr, 0, false);
+
+  success = (this->read_register(*pin, this->read_buffer_, 1) == i2c::ERROR_OK);
 
   if (!success) {
     this->status_set_warning();
